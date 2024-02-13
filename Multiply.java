30659d5e35be96de17f67f44b119cf09cc2197e9
Nagesh R <nageshrandhave75@gmail.com>
2024-02-14 00:28:58 +0530

    Added multiply.java

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+public class Multiply {
+    public static void main(String args[]){
+        int a =10;
+        int b=20;
+        int product = a*b;
+        System.out.println(product);
+    }
+
+}
